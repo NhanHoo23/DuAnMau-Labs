d4583af12adbcee820e1246bd584a9af25534576
NhanHoo23 <hohuunhan2001@gmail.com>
2024-05-20 00:01:01 +0700

    Create main activity

--- app/src/main/java/fpoly/nhanhhph47395/pnlib/MainActivity.java
+++ app/src/main/java/fpoly/nhanhhph47395/pnlib/MainActivity.java
@@ -1,20 +1,89 @@
 package fpoly.nhanhhph47395.pnlib;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
-import fpoly.nhanhhph47395.pnlib.dao.DemoDB;
+
+import com.google.android.material.navigation.NavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
+    private View mHeaderView;
+    private TextView edUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Tạo db
-        DemoDB demoDB = new DemoDB(getApplicationContext());
-        demoDB.thanhVien();
+        drawer = findViewById(R.id.drawer_layout);
+        toolbar = findViewById(R.id.toolbar1);
+
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        NavigationView nv = findViewById(R.id.nvView);
+        mHeaderView = nv.getHeaderView(0);
+        edUser = mHeaderView.findViewById(R.id.txtUser);
+        Intent i = getIntent();
+        edUser.setText("Welcome!");
+
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentManager manager = getSupportFragmentManager();
+
+                if (item.getItemId() == R.id.nav_PhieuMuon) {
+                    setTitle("Quản lý Phiếu Mượn");
+                } else if (item.getItemId() == R.id.nav_LoaiSach) {
+                    setTitle("Quản lý Loại Sách");
+                } else if (item.getItemId() == R.id.nav_Sach) {
+                    setTitle("Quản lý Sách");
+                } else if (item.getItemId() == R.id.nav_ThanhVien) {
+                    setTitle("Quản lý Thành Viên");
+                } else if (item.getItemId() == R.id.sub_Top) {
+                    setTitle("Top 10 sách cho thuê nhiều nhất");
+                } else if (item.getItemId() == R.id.sub_DoanhThu) {
+                    setTitle("Thống kê doanh thu");
+                } else if (item.getItemId() == R.id.sub_AddUser) {
+                    setTitle("Thêm người dùng");
+                } else if (item.getItemId() == R.id.sub_Pass) {
+                    setTitle("Đổi mật khẩu");
+                } else if (item.getItemId() == R.id.sub_Logout) {
+
+                } else {
+
+                }
+
+                drawer.closeDrawers();
+
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            drawer.openDrawer(GravityCompat.START);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/fpoly/nhanhhph47395/pnlib/dao/DemoDB.java
+++ app/src/main/java/fpoly/nhanhhph47395/pnlib/dao/DemoDB.java
@@ -23,12 +23,15 @@ public class DemoDB {
     public void thanhVien() {
 
         ThanhVien tv = new ThanhVien(1, "Nguyen Van A", "2001");
-        if (thanhVienDao.insert(tv) > 0) {
-            Log.i("//=====", "Them thanh cong");
-        } else {
-            Log.i("//=====", "Them that bai");
-        } 
+//        if (thanhVienDao.insert(tv) > 0) {
+//            Log.i("//=====", "Them thanh cong");
+//        } else {
+//            Log.i("//=====", "Them that bai");
+//        }
 
+        Log.i("//=====", "Tong so thanhVien: " + thanhVienDao.getAll().size());
+        tv = new ThanhVien(1, "Nguyen B", "2000");
+        thanhVienDao.update(tv);
         Log.i("//=====", "Tong so thanhVien: " + thanhVienDao.getAll().size());
     }
 }
